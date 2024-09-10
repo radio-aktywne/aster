36655a1fbd6c743603e5f32216e33f6cafca0e66
Sebastian Pietras <me@spietras.dev>
2024-09-10 23:24:00 +0200

    Fixed `emifuse` port (#2)

--- src/api/emifuse/client.ts
+++ src/api/emifuse/client.ts
@@ -7,7 +7,7 @@ const scheme = process.env.WEBFUSE__EMIFUSE__HTTP__SCHEME || "http";
 const host = process.env.WEBFUSE__EMIFUSE__HTTP__HOST || "localhost";
 const port =
   process.env.WEBFUSE__EMIFUSE__HTTP__PORT === undefined
-    ? 9000
+    ? 9001
     : process.env.WEBFUSE__EMIFUSE__HTTP__PORT;
 const path = (process.env.WEBFUSE__EMIFUSE__HTTP__PATH || "")
   // Ensure path starts with a slash
