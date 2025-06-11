95ff9878c5345724d0251192f4ccc8f738e31d14
Sebastian Pietras <me@spietras.dev>
2025-06-11 20:52:27 +0000

    Disabled cache for dynamic segments

--- src/lib/dingo/get-current-playlist/main.ts
+++ src/lib/dingo/get-current-playlist/main.ts
@@ -5,7 +5,9 @@ import { DingoError } from "../errors";
 import { GetCurrentPlaylistInput, GetCurrentPlaylistOutput } from "./types";
 
 export async function getCurrentPlaylist({}: GetCurrentPlaylistInput = {}): Promise<GetCurrentPlaylistOutput> {
-  const { data, error, response } = await dingo.GET("/playlist");
+  const { data, error, response } = await dingo.GET("/playlist", {
+    cache: "no-store",
+  });
 
   if (error || !data || !response.ok) {
     if (response.status === 404) return { playlist: null };
