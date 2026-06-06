d3753904094506ad1a9167443a2cf8422fd30ac8
Sebastian Pietras <me@spietras.dev>
2026-06-06 14:42:34 +0200

    Fetched template changes (#19)

--- src/server/orpc/vars/procedures/vars/core/vars/list-playlists/main.ts
+++ src/server/orpc/vars/procedures/vars/core/vars/list-playlists/main.ts
@@ -3,9 +3,11 @@ import { isJSONValue } from "es-toolkit/predicate";
 
 import { state } from "../../../../../../../state/vars/state";
 import { orpcServerRootBase } from "../../../../../bases/root";
+import { authenticatedMiddleware } from "../../../../../middleware/authenticated";
 
-export const listPlaylists = orpcServerRootBase.core.listPlaylists.handler(
-  async ({ errors, input }) => {
+export const listPlaylists = orpcServerRootBase.core.listPlaylists
+  .use(authenticatedMiddleware)
+  .handler(async ({ errors, input }) => {
     const { data: playlistsListData } =
       await state.current.apis.pelican.playlistsList({
         query: mapValues(input ?? {}, (value) =>
@@ -16,5 +18,4 @@ export const listPlaylists = orpcServerRootBase.core.listPlaylists.handler(
     if (playlistsListData === undefined) throw errors.INTERNAL_SERVER_ERROR();
 
     return playlistsListData;
-  },
-);
+  });
--- src/server/orpc/vars/procedures/vars/core/vars/remove-current-playlist/main.ts
+++ src/server/orpc/vars/procedures/vars/core/vars/remove-current-playlist/main.ts
@@ -1,10 +1,14 @@
 import { state } from "../../../../../../../state/vars/state";
 import { orpcServerRootBase } from "../../../../../bases/root";
+import { authenticatedMiddleware } from "../../../../../middleware/authenticated";
 
 export const removeCurrentPlaylist =
-  orpcServerRootBase.core.removeCurrentPlaylist.handler(async ({ errors }) => {
-    const { data: deletePlaylistData } =
-      await state.current.apis.dingo.deletePlaylist();
+  orpcServerRootBase.core.removeCurrentPlaylist
+    .use(authenticatedMiddleware)
+    .handler(async ({ errors }) => {
+      const { data: deletePlaylistData } =
+        await state.current.apis.dingo.deletePlaylist();
 
-    if (deletePlaylistData === undefined) throw errors.INTERNAL_SERVER_ERROR();
-  });
+      if (deletePlaylistData === undefined)
+        throw errors.INTERNAL_SERVER_ERROR();
+    });
